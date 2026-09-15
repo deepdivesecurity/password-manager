552b7d7be0a38f33ec0604dfea7bc4fe39d13704
deepdivesecurity <165413290+deepdivesecurity@users.noreply.github.com>
2026-09-15 19:49:58 -0400

    basic file saving

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from tkinter import *
+from pathlib import Path
+from urllib.parse import urlparse
+
+# ---------------------------- HELPER FUNCTIONS ------------------------------- #
+def parse_url(website: Entry) -> str: 
+    url = website.get().strip()
+
+    if not url.startswith(("http://", "https://")):
+        url_to_parse = f"http://{url}"
+    else:
+        url_to_parse = url
+
+    domain = urlparse(url_to_parse).netloc
+
+    if domain.startswith("www."):
+        domain = domain[4:]
+
+    return domain.split('.')[0]
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def gen_password(): 
+    pass
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password(website: Entry, email: Entry, password: Entry) -> None: 
+    file_path = Path("data/passwords.txt")
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    
+    with open(file_path, "a") as file: 
+        file.write(f"{website.get()} | {email.get()} | {password.get()}\n")
+
+    website.delete(0, END)
+    email.delete(0, END)
+    password.delete(0, END)
+    website.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -29,6 +60,7 @@ def main() -> None:
     # Text Boxes
     web_txt = Entry(width=35)
     web_txt.grid(row=1, column=1, columnspan=2)
+    web_txt.focus()
 
     eml_txt = Entry(width=35)
     eml_txt.grid(row=2, column=1, columnspan=2)
@@ -40,7 +72,7 @@ def main() -> None:
     btn_gen_pas = Button(text="Generate Password")
     btn_gen_pas.grid(row=3, column=2)
 
-    btn_add = Button(text="Add", width=36)
+    btn_add = Button(text="Add", width=36, command=lambda: save_password(web_txt, eml_txt, pas_txt))
     btn_add.grid(row=4, column=1, columnspan=2)
     
     window.mainloop()
