3a44e8b02d78e649505ed1b2d5583ec76db74662
deepdivesecurity <165413290+deepdivesecurity@users.noreply.github.com>
2026-09-15 10:52:01 -0400

    basic background gui

--- main.py
+++ main.py
@@ -7,7 +7,18 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 
 def main() -> None:
-    pass
+    window = Tk()
+    window.title("Password Manager")
+    window.config(padx=20, pady=20)
+
+    canvas = Canvas(window, width=200, height=200, bg="white", highlightthickness=0)
+    bg_image = PhotoImage(file="assets/logo.png")
+    canvas.create_image(100, 100, image=bg_image)
+
+    canvas.pack(padx=20, pady=20)
+    
+    window.mainloop()
+    
 
 if __name__ == "__main__":
     main()
